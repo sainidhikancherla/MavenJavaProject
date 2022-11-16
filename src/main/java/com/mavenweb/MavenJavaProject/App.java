f48ebadeb97f621930e89c50943a96ffd12263e2
sainidhikancherla <sainireddy.2003@gmail.com>
2022-11-16 13:14:02 +0530

    Maven Java Project push

---
+++ src/main/java/com/mavenweb/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from 20BD1A66A7-SAINIDHI-(CSMB)" );
+    }
+}
